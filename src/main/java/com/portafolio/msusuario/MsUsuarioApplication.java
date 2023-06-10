95c0ec4f11ecb2342c58ab8b241e5f8c10827d49
josemariapafo <parrillajosemaria@gmail.com>
2023-06-10 13:02:04 +0200

    Hacemos trabajar al micro con eureka

--- src/main/java/com/portafolio/msusuario/MsUsuarioApplication.java
+++ src/main/java/com/portafolio/msusuario/MsUsuarioApplication.java
@@ -2,10 +2,12 @@ package com.portafolio.msusuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsUsuarioApplication {
 
 	public static void main(String[] args) {
